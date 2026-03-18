828e7c2c49f2356f5ecd79ce6acf38ccf409d167
gyornorbert <gyor.norbert@students.jedli.eu>
2026-03-18 08:14:03 +0100

    Enemy Hp fixed

--- src/entities/tank_enemy.py
+++ src/entities/tank_enemy.py
@@ -4,7 +4,7 @@ from src.entities.enemy import Enemy, LILA
 class TankEnemy(Enemy):
     def __init__(self, path):
         # Lassú, sok élet
-        super().__init__(path, speed=1.5, hp=30, radius=20, color=LILA)
+        super().__init__(path, speed=1.5, hp=60, radius=20, color=LILA)
 
     def draw_shape(self, surface):
         # ÚJ: A Tank egy négyzet ("kocka")
